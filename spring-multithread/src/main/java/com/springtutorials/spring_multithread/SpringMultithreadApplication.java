f483991ad29ee00e42fafa32bfa83191833a9f43
apiljugin <apiljugin@datas-tech.com>
2024-07-27 17:59:22 +0300

    add spring-mvc & spring-multithread modules

---
+++ spring-multithread/src/main/java/com/springtutorials/spring_multithread/SpringMultithreadApplication.java
@@ -0,0 +1,13 @@
+package com.springtutorials.spring_multithread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMultithreadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMultithreadApplication.class, args);
+	}
+
+}
