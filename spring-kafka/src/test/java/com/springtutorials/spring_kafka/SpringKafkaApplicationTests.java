7c14b50dbd09d29d9c3509597ee8e8d0af54f5a7
apiljugin <apiljugin@datas-tech.com>
2024-07-30 12:53:07 +0300

    add spring-kafka module

---
+++ spring-kafka/src/test/java/com/springtutorials/spring_kafka/SpringKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springtutorials.spring_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
